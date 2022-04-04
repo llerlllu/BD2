192f172f6c924e18732dab9a6d60ec1692704e9b
llerlllu <llerlllu@gmail.com>
2022-04-04 09:28:18 +0300

    v1

---
+++ BD2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BD2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Login = "admin";
+            var Parol = "admin";
+
+            if ((Log.Text == Login) && (Par.Password == Parol))
+            {
+                Window neww = new Window1();
+                neww.Show();
+                Close();
+            }
+            if ((Log.Text == Login) && (Par.Password != Parol))
+            {
+                MessageBox.Show("Не верный пароль");
+            }
+            if (Log.Text != Login)
+            {
+                MessageBox.Show("Не верный логин");
+            }
+        }
+    }
+}
---
+++ BD2/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BD2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        PraktikaEntities Pr = new PraktikaEntities();
+        public Window1()
+        {
+            InitializeComponent();
+            dataGrid.ItemsSource = Pr.ID_FIO.ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
